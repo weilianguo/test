244da1d46b0b0e0ca7fc24211d124ba32f26cf06
nie <1063551195@qq.com>
2022-08-11 17:29:25 +0800

    更新代码

--- lI_udp_client/II_udp_client/main.c
+++ lI_udp_client/II_udp_client/main.c
@@ -25,27 +25,37 @@
 #include "msg.h"
 
 #define MAIN_QUEUE_SIZE     (8)
-#define SERVER_IP			"fe80::e4be:307:f6a:e836"
 #define SERVER_PORT			123
 
-static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 extern void send(char *addr_str, uint16_t port, char *data, int len);
 
+static int udp_cmd(int argc, char **argv)
+{
+    if (argc != 2) {
+        printf("udp_send <IPv6 address>\n");
+        return -1;
+    }
+	char str[] = "Hello IEEE 802.15.4!";
+	for (int i = 0; i < 10; i++) {
+		printf("send %d packet\n", i+1);
+		send(argv[1], SERVER_PORT, str, strlen(str));
+		xtimer_msleep(1000);
+	}
+    return 0;
+}
 
+static const shell_command_t shell_commands[] = {
+    { "udp_send", "send data over UDP and listen on UDP ports", udp_cmd },
+    { NULL, NULL, NULL }
+};
 
 int main(void)
 {
     /* we need a message queue for the thread running the shell in order to
      * receive potentially fast incoming networking packets */
-    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     puts("RIOT network stack example application");
-    printf("server ip= %s\n", SERVER_IP);
-	char str[] = "1234567890";
-	for (int i = 0; i < 10; i++) {
-		printf("send %d packet\n", i+1);
-		send(SERVER_IP, SERVER_PORT, str, strlen(str));
-		xtimer_msleep(1000);
-	}
+    char buf[512];
+    shell_run(shell_commands, buf, SHELL_DEFAULT_BUFSIZE);
     /* should be never reached */
     return 0;
 }
